db5e61a8db5712ec141148d539650efbf5a0662a
Chris Rowe <rowe2ry@yahoo.com>
2022-04-05 16:31:49 -0400

    API calls 2 and 3 are linked togehter and have been debugged

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ var geoApifyAPIkey = 'ea553f8b84ca463ebd4f9b51790b865e';
 var commaInsertion = '%2C';
 var pipeInsertion = '%7C';
 var ipAddressURL ='https://ipinfo.io/json?token=fc2c5059a0b866';
-var travelType ='drive';
 var startingLatLon = [33.87820,-84.23729]; // plug in from user's IP address fetch
 var endingLatLon = [28.54064,-81.36489 ]; // plug in from weather API search
 
@@ -40,11 +39,11 @@ function fetchingUserLocation () {
 };
 
 // grab lat lon coordinate data from OpenWeather by city name
-function useWeathertoGetcoords(location) {
+function useWeathertoGetcoords(str,startLat,startLon) {
     // API url, the city is entered by the user
-    var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + 
-    location + 
-    '&appid=' +
+    var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + // base API cll url
+    str + // city by name
+    '&appid=' + // string query for the key
     APIkeyCurrent;
 
     // grabs the raw data we need
@@ -53,12 +52,12 @@ function useWeathertoGetcoords(location) {
             if (response.ok) {
                 response.json() // format the data into a usable and mutable object
                     .then(function (data) {
-                        var nowName = data.name; //  Grab the city name --- string --- index 0
-                        var latitude = data.coord.lat; // Grab latitude coordinate
-                        var longitude = data.coord.lon; // Grab longitude coordinate
-                        var nowDataRelevant = [
-                            nowName, latitude, longitude
-                        ]; // just the 3 pieces of info we needed from the current Weather API call which uses a city name to search
+                        var endLat = data.coord.lat; // Grab latitude coordinate
+                        var endLon = data.coord.lon; // Grab longitude coordinate
+                        var coordinates = [
+                            startLat, startLon, endLat, endLon
+                        ]; // just the 4 pieces of info we needed from the current Weather API call which uses a city name to search
+                        grabRoute(coordinates[0],coordinates[1],coordinates[2],coordinates[3])
                     });
             } else {
                 console.log('error with current weather fetch | likely invalid city name'); // just some feedback if the API doesn't recognize the city name
@@ -66,15 +65,16 @@ function useWeathertoGetcoords(location) {
         });
 };
 
-function grabRoute() {
+function grabRoute(startLat, startLon, endLat, endLon) {
+    var travelType ='drive'; // also could be "walk" or "approximated_transit"
     fetch('https://api.geoapify.com/v1/routing?waypoints=' + 
-            startingLatLon[0] + // starting location latitude
+            startLat + // starting location latitude
             commaInsertion + // a comma ,
-            startingLatLon[1] + // starting location longitude
+            startLon + // starting location longitude
             pipeInsertion + // a pipe operator |
-            endingLatLon[0] + // finishing location latitude
+            endLat + // finishing location latitude
             commaInsertion + // a comma ,
-            endingLatLon[1] + // finishing location longitude
+            endLon + // finishing location longitude
             '&mode=' + // string query for method of transportation
             travelType +  // walking || flying || public transit || * driving
             '&&units=imperial&apiKey=' + // use miles instead of meters and query for the API key
@@ -84,6 +84,8 @@ function grabRoute() {
           response.json() // format the data into a usable and mutable object
               .then(function (data) { // and do stuff
                   console.log(data); // in this case just log to console
+                  driveDistance = data.features[0].properties.distance;
+                  console.log(driveDistance);
               });
           } else {
               console.log('error with current fetch'); // just some feedback if the API doesn't recognize the city name
@@ -100,5 +102,5 @@ function grabRoute() {
  * ========================================================================= */
 
 fetchingUserLocation ();
-grabRoute();
+grabRoute(startingLatLon[0],startingLatLon[1],endingLatLon[0],endingLatLon[1]);
 
